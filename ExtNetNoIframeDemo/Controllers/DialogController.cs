45178ff62288e580ad267caced281b11a6c88df9
josmonver <josmonver@gmail.com>
2016-04-26 09:53:35 +0200

    Add dialog window

---
+++ ExtNetNoIframeDemo/Controllers/DialogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ExtNetNoIframeDemo.Controllers
+{
+    public class DialogController : Controller
+    {
+        // GET: Dialog/Index
+        public Ext.Net.MVC.PartialViewResult Index(string containerId)
+        {
+            return new Ext.Net.MVC.PartialViewResult(containerId)
+            {
+                ClearContainer = true,
+                WrapByScriptTag = false,
+                ViewName = "_Dialog"
+            };
+        }
+    }
+}
